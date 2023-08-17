4893fc52c68474502afd9bd9af02735210ee3b07
hisachin2000 <142435968+hisachin2000@users.noreply.github.com>
2023-08-17 09:20:43 -0500

    Update tstfile.py

--- tstfile.py
+++ tstfile.py
@@ -1,7 +1,4 @@
 import streamlit
-import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
 
 streamlit.title('My Parents new healthy Diner')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
@@ -10,3 +7,7 @@ streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
+
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
