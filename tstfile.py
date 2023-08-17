b0026486570e0e9c078c2f9f3c3a256243b80df0
hisachin2000 <142435968+hisachin2000@users.noreply.github.com>
2023-08-17 09:19:57 -0500

    Update tstfile.py

--- tstfile.py
+++ tstfile.py
@@ -1,4 +1,7 @@
 import streamlit
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
 
 streamlit.title('My Parents new healthy Diner')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
