358f9342398dc286e8ac7648f3ec9feb244614bf
hisachin2000 <142435968+hisachin2000@users.noreply.github.com>
2023-08-17 09:22:02 -0500

    Update tstfile.py

--- tstfile.py
+++ tstfile.py
@@ -2,7 +2,7 @@ import streamlit
 
 streamlit.title('My Parents new healthy Diner')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-
+streamlit.header("Fruityvice Fruit Advice!")
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
